00232ded30f11ced6514971abd0aed4dfb01b007
Gabriel Evangelista dos Santos <68117644+GabrielEvangelista06@users.noreply.github.com>
2022-09-28 15:11:13 -0300

    Código refatorado e mudanças nas cores

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,45 +14,6 @@ function startInterval() {
   }, 1000)
 }
 
-document.addEventListener('click', function (e) {
-  const el = e.target
-
-  if (el.classList.contains('comecar')) {
-    stopInterval()
-    startInterval()
-
-    comecar.innerHTML = 'Pausar'
-    comecar.classList.add('pausar')
-    comecar.classList.remove('comecar')
-  } else if (el.classList.contains('pausar')) {
-    stopInterval()
-    
-    comecar.innerHTML = 'Começar'
-    comecar.classList.add('comecar')
-    comecar.classList.remove('pausar')
-  } else if (el.classList.contains('zerar')) {
-    horas.value = 0
-    minutos.value = 0
-    segundos.value = 0
-
-    comecar.innerHTML = 'Começar'
-
-    comecar.classList.add('comecar')
-    comecar.classList.remove('pausar')
-
-    stopInterval()
-  }
-})
-
-document.addEventListener('keypress', function (event) {
-  if (event.key === 'i') {
-    stopInterval()
-    startInterval()
-  } else if (event.key === 'p') {
-    stopInterval()
-  }
-})
-
 function timer() {
   if (horas.value == 0 && minutos.value == 0 && segundos.value == 0) {
     horas.value = 0
@@ -72,6 +33,47 @@ function timer() {
   return
 }
 
+function transformaBotaoEmComecar() {
+  comecar.innerHTML = 'Começar'
+  comecar.classList.add('comecar')
+  comecar.classList.remove('pausar')
+}
+
+function transformaBotaoEmPausar() {
+  comecar.innerHTML = 'Pausar'
+  comecar.classList.add('pausar')
+  comecar.classList.remove('comecar')
+}
+
 function stopInterval() {
   clearInterval(interval)
 }
+
+document.addEventListener('click', function (e) {
+  const el = e.target
+
+  if (el.classList.contains('comecar')) {
+    startInterval()
+    transformaBotaoEmPausar()
+  } else if (el.classList.contains('pausar')) {
+    stopInterval()
+    transformaBotaoEmComecar()
+  } else if (el.classList.contains('zerar')) {
+    horas.value = 0
+    minutos.value = 0
+    segundos.value = 0
+    stopInterval()
+    transformaBotaoEmComecar()
+  }
+})
+
+document.addEventListener('keypress', function (event) {
+  if (event.key === 'i') {
+    stopInterval()
+    startInterval()
+    transformaBotaoEmPausar()
+  } else if (event.key === 'p') {
+    stopInterval()
+    transformaBotaoEmComecar()
+  }
+})
